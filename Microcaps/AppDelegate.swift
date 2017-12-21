f97d1db76d1efb428902797a63c68afabcc1bc8e
Sidney de Koning <sidney@thecapitals.nl>
2017-12-21 16:25:40 +0100

    Initial Commit

---
+++ Microcaps/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Microcaps
+//
+//  Created by Sidney de Koning on 21/12/2017.
+//  Copyright © 2017 Funky Monkey. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Microcaps/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Microcaps
+//
+//  Created by Sidney de Koning on 21/12/2017.
+//  Copyright © 2017 Funky Monkey. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
